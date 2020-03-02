4802f1387fbb616e74c5b440cef046aa8f6c4cc1
Mqodir <61658773+Mqodir@users.noreply.github.com>
2020-03-02 04:33:29 +0300

    Add files via upload

---
+++ js/constants.js
@@ -0,0 +1,37 @@
+
+this.colors = {"active" : "#ffad71",
+				"selected" : "#ffdabd",
+               	"inactive" : "#ff7000"};
+
+
+this.colorsCatrgory = {"active" : "#ffdabd",
+               		"inactive" : "#fff6ef"};
+
+this.colorsSearch = {"active" : "#ffffff",
+               "inactive" : "#fff6ef"};
+
+this.colorsSearchBorder = {"active" : "#555555",
+               "inactive" : "#fff6ef"};
+
+this.poster = {"width"  : 172,
+               "height" : 264};
+
+this.itemTop = {"width"  : 220,
+               "height" : 240};
+
+
+this.borderSearch = {"active"  : 1,
+               "inactive" : 0};
+
+this.menuWidth = 256;
+
+this.inactiveOpacity = 0.8;
+
+this.animationDuration = 150;
+
+this.margin = 60;
+
+this.defaultPoster = "apps/icook/resources/icon200.png";
+
+
+this.itemViewInfo = "Значение приведено для диеты, основанной на 2500 ккал/сутки.";
